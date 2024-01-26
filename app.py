5228a022590f4b49aea7875e3d0251e5eacb387f
cantseeathing <mahmoued_999@hotmail.com>
2024-01-26 15:09:28 +0300

    db finalized

--- app.py
+++ app.py
@@ -1,17 +1,135 @@
-from flask import Flask, render_template
-from helper import Helper
+from flask import Flask, render_template, url_for, g, request, redirect
+from dotenv import load_dotenv
+import sqlite3
+import os
+import helper as helper
+
+load_dotenv(verbose=True)
 
 app = Flask(__name__)
 
-
-with app.app_context():
-    helper = Helper()
+DATABASE = os.getenv('DB')
 
 
-@app.route('/')
+def get_db():
+    """
+    Opens a new database connection if there is none
+    :return: database connection instance
+    """
+    # # CHECK IF THE DB INSTANCE EXIST OR NOT
+    db = getattr(g, '_database', None)
+    if db is None:
+        # # CONNECT TO THE DB
+        db = g._database = sqlite3.connect(DATABASE)
+        # # GET BACK THE DATA FROM THE DB IN ROW FORMAT
+        db.row_factory = sqlite3.Row
+        # # CREATE THE DB IF IT DOES NOT EXIST
+        helper.create_tables(db=db)
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+@app.route('/', methods=['GET'])
 def home():
-    return render_template('home.html', title='Home')
+    # # GET THE DB INSTANCE
+    db = get_db()
+    # # QUERY ALL THE DAYS FROM THE INTAKE TABLE IN THE DB
+    all_days = helper.query_all_days(db=db)
+    print([dict(x) for x in helper.query_all_foods(db=db)])
+    print(all_days)
+    days_list = []
+    print('here')
+    # # IF THE DB RETURN INTAKE DATES
+    if len(all_days) != 0:
+        # # CONVERT THE DATA TO A LIST OF DICTIONARIES
+        for days in all_days:
+            print('days: ', days)
+            days = dict(days)
+            aggregate = helper.aggregate_values(db=db, result=[days])
+            days.update(aggregate)
+            days_list.append(days)
+        print(days_list)
+    return render_template('home.html',
+                           title='Food Tracker App',
+                           days_list=days_list)
+
+
+@app.route('/add_food', methods=['GET', 'POST'])
+def add_food():
+    """
+    Add food to the food table in the database
+    """
+    # # GET DB INSTANCE
+    db = get_db()
+    # # POST REQUEST
+    if request.method == 'POST':
+        # # CHECK IF THE FORM RETURNS FOOD ID FIELD THEN THE USER WANTS TO EDIT FOOD VALUES
+        if request.form.get('food_id') is not None:
+            print(request.form.get('food_id'))
+        # # NO FOOD ID FIELD
+        else:
+            # # GET THE FOOD NAME, PROTEIN, CARBS, AND FAT VALUE FROM THE FORM
+            food_name = request.form.get('food_name', type=str, default='Unnamed Food')
+            food_protein = request.form.get('food_protein', type=int, default=0)
+            food_carbs = request.form.get('food_carbs', type=int, default=0)
+            food_fat = request.form.get('food_fat', type=int, default=0)
+            # # INSERT THE FOOD TO THE FOODS TABLE
+            helper.insert_food(food_name, food_protein, food_carbs, food_fat, db=db)
+        # # RETURN THE USER TO THE ADD FOOD PAGE
+        return redirect(url_for('add_food'))
+    # # GET REQUEST
+    else:
+        # # QUERY THE DB FOR ALL THE FOOD TYPES
+        foods = helper.query_all_foods(db=db)
+        return render_template('add_food.html',
+                               title='Add Food',
+                               foods=foods)
+
+
+@app.route('/add_day', methods=['GET', 'POST'])
+def add_day():
+    db = get_db()
+    if request.method == 'POST':
+        # # REMOVE FOOD FROM A DATE INTAKE
+        if request.form.get('food_id') is not None:
+            date = request.form.get('date', type=str)
+            food_id = request.form.get('food_id', type=int)
+            print('here', date, food_id)
+            helper.delete_food(db=db, id=food_id, date=date)
+            return redirect(url_for('add_day', date=date))
+        # # ADD FOOD TO A DATE INTAKE
+        food_id = request.form.get('food', type=str)
+        date = request.form.get('date', type=str)
+        helper.insert_intake(db=db, food_id=food_id, date=date)
+        print(food_id, date)
+        return redirect(url_for('add_day', date=date))
+    date = request.args.get('date', type=str, default=None)
+    # # NO DATE WAS PROVIDED BY THE USER
+    if date is None or date == '':
+        # # REDIRECT THE USER BACK TO THE HOME PAGE
+        return redirect(url_for('home'))
+    # # QUERY THE DB FOR THE PROVIDED DATE
+    result = helper.query_db_date(db=db, date=date)
+    # print("date: ", date, " intake", dict(result[0]))
+    # # QUERY THE DB FOR ALL THE AVAILABLE FOODS
+    foods = helper.query_all_foods(db=db)
+    print("all foods: ", foods)
+    print("date result: ", helper.convert_result_to_dict(result))
+    aggregate = helper.aggregate_values(db=db, result=result)
+    print(aggregate)
+    return render_template('add_day.html',
+                           title='Add Day',
+                           date=date,
+                           foods=foods,
+                           result=result,
+                           aggregate=aggregate)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- helper.py
+++ helper.py
@@ -1,16 +1,160 @@
-import sqlite3
-from flask import g
-
-DATABASE = 'food_tracker.db'
+from typing import List, Dict
 
 
-class Helper:
-    def __init__(self):
-        self.db = self.get_db()
+def create_tables(db):
+    """
+    Creates the tables in the database if they don't already exist
+    :param db: DB connection instance
+    :return:
+    """
+    db.execute('''
+        CREATE TABLE IF NOT EXISTS FOODS
+            ( id INTEGER PRIMARY KEY,
+              name TEXT NOT NULL,
+              protein INTEGER NOT NULL,
+              carbohydrates INTEGER NOT NULL,
+              fat INTEGER NOT NULL )
+    ''')
+    db.execute('''
+        CREATE TABLE IF NOT EXISTS INTAKE
+            ( date TEXT NOT NULL,
+              id INTEGER PRIMARY KEY,
+              food_id INTEGER NOT NULL REFERENCES FOODS(id) )
+    ''')
+    # # COMMIT THE CHANGES TO THE DATABASE
+    db.commit()
 
-    @staticmethod
-    def get_db():
-        db = getattr(g, '_database', None)
-        if db is None:
-            db = g._database = sqlite3.connect(DATABASE)
-        return db
+
+def query_all_days(db) -> List:
+    """
+    Queries all days in the INTAKE table and returns the results
+    :param db: DB connection instance
+    :return: List of all results
+    """
+    cursor = db.execute("""
+        SELECT * FROM INTAKE;
+    """)
+    # # FETCH ALL THE RESULTS
+    results = cursor.fetchall()
+    return results
+
+
+def query_db_date(db, date: str) -> List:
+    """
+    Queries the INTAKE table for a specific date and returns the results as list
+    :param db: DB connection instance
+    :param date: Date to be queried as a string
+    :return: List of all results
+    """
+    cursor = db.execute(f"""
+        SELECT * FROM INTAKE 
+            WHERE date = '{date}';
+    """)
+    # # FETCH ALL RESULTS
+    results = cursor.fetchall()
+    return results
+
+
+def convert_result_to_dict(results: List) -> List:
+    """
+    Converts the results from the DB query to a list of dictionaries
+    :param results: A list of sqlite3.Row objects
+    :return: List of dictionaries
+    """
+    if len(results) == 0 or results[0] is None:
+        return []
+    return [dict(x) for x in results]
+
+
+def query_all_foods(db) -> List:
+    """
+    Queries all food types from the FOODS table and returns the results
+    :param db: DB connection instance
+    :return: List of all food
+    """
+    cursor = db.execute("SELECT * FROM FOODS")
+    # # FETCH ALL THE RESULTS
+    results = cursor.fetchall()
+    return results
+
+
+def insert_food(*args, **kwargs):
+    """
+    Inserts a new food type into the FOODS table
+    :param args: name, protein, carbohydrate, fat values of the food type
+    :param kwargs: db: DB connection instance
+    :return:
+    """
+    db = kwargs.get('db')
+    db.execute("""
+        INSERT INTO FOODS (name, protein, carbohydrates, fat)
+        VALUES (?, ?, ?, ?)
+    """, [args[0], args[1], args[2], args[3]])
+    # # COMMIT THE CHANGES TO THE DB
+    db.commit()
+
+
+def insert_intake(**kwargs):
+    """
+    Inserts a new food intake into the INTAKES table
+    :param kwargs: db: DB connection instance, date: to query intake table, food_id: food id from the foods table
+    :return:
+    """
+    db = kwargs.get('db')
+    date = kwargs.get('date')
+    food_id = kwargs.get('food_id')
+    db.execute("""
+        INSERT INTO INTAKE (date, food_id)
+        VALUES (?, ?)
+    """, [date, food_id])
+    db.commit()
+
+
+def query_food(db, food_id):
+    cursor = db.execute(f"""
+        SELECT * FROM FOODS
+            WHERE id = '{food_id}';
+    """)
+    results = cursor.fetchone()
+    return results
+
+
+def delete_food(db, id: int, date: str):
+    """
+    Deletes food from the INTAKES table
+    :param db: DB connection instance
+    :param id: id in the INTAKES table
+    :param date: date to query the INTAKE table
+    :return: 
+    """
+    db.execute(f"""
+        DELETE FROM INTAKE WHERE id = '{id}' AND date = '{date}';
+    """)
+    db.commit()
+
+
+def aggregate_values(db, result: List) -> Dict:
+    """
+    Aggregates the total nutrition values for the list of foods given
+    :param db: DB connection instance
+    :param result: List of foods to aggregate
+    :return: Dictionary with the total nutrition values
+    """
+    agg_values = {
+        "count": 0,
+        "fat": 0,
+        "protein": 0,
+        "carbs": 0
+    }
+    if len(result) == 0 or result[0] is None:
+        return agg_values
+    for food in convert_result_to_dict(result):
+        print('food: ', food)
+        temp_food = dict(query_food(db=db, food_id=food.get('food_id')))
+        agg_values["count"] += 1
+        agg_values["fat"] += temp_food.get('fat')
+        agg_values["protein"] += temp_food.get('protein')
+        agg_values["carbs"] += temp_food.get('carbohydrates')
+    return agg_values
+
+
